4ecf816b00ec74522326afedd0417cf260b7ba0a
Bruno da Silva João <brunodasj@gmail.com>
2013-09-03 13:09:09 -0300

    melhorado form de alteracao de senha

--- application/views/user/changePassword.php
+++ application/views/user/changePassword.php
@@ -4,15 +4,15 @@
 	<div class="span8">
 		senha atual
 		<div class="control-group">
-			<input type="text" id="passwdOld" class="input-block-level" />
+			<input type="password" id="passwdOld" class="input-block-level" />
 		</div>
 		nova senha
 		<div class="control-group">
-			<input type="text" id="passwdNew" class="input-block-level" />
+			<input type="password" id="passwdNew" class="input-block-level" />
 		</div>
 		nova senha - confirmação
 		<div class="control-group">
-			<input type="text" id="passwdNewConf" class="input-block-level" />
+			<input type="password" id="passwdNewConf" class="input-block-level" />
 		</div>
 		<a id="changePassword" class="btn btn-primary">alterar senha</a>
 	</div>
