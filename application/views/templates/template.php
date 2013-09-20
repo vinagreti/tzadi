22de3eee9199275a34b42136b3f9a5d095e94e29
bruno <bruno@vm-ubuntu-64.(none)>
2013-09-20 14:00:05 -0300

    possibilitando adicoinar conteudo ao <head>

--- application/views/templates/template.php
+++ application/views/templates/template.php
@@ -25,7 +25,7 @@
 
     <meta property="fb:admins" content="100000036122524,1110189261"/>
     <meta property="og:app_id" content="532888376778430"/>
-    <meta property="og:type" content="website" />
+
     <meta property="og:video" content="https://www.youtube.com/embed/mruP0yf7wvw" />
 
     {the_head}
