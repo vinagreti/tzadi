bda37720bacc3144d3403877bc304ae7d5c37088
bruno <bruno@vm-ubuntu-64.(none)>
2013-09-20 14:49:56 -0300

    mostrar vido og:video no face

--- application/views/templates/template.php
+++ application/views/templates/template.php
@@ -27,8 +27,8 @@
     <meta property="fb:admins" content="100000036122524"/> <!-- Bruno Facebook ID -->
     <meta property="fb:app_id" content="532888376778430"/>
     <meta property="og:type" content="website" />
-    <meta property="og:video" content="http://www.youtube.com/v/mruP0yf7wvw?version=3&amp;autohide=1">
-    <meta property="og:video:type" content="application/x-shockwave-flash">
+    <meta property="og:video" content="https://www.youtube.com/watch?v=mruP0yf7wvw" />
+    
     <meta property="og:video:secure_url" content="https://www.youtube.com/watch?v=mruP0yf7wvw" />
 
     {the_head}
